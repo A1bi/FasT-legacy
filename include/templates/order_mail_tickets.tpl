dbf391f21c685da7eb4b5a75b51dcd2fbf178751
Albrecht Oster <a.oster@online.de>
2012-05-31 14:32:24 +0200

    create pdf document containing the tickets

---
+++ include/templates/order_mail_tickets.tpl
@@ -0,0 +1,9 @@
+Hallo {$order['address']['firstname']} {$order['address']['lastname']}!
+
+Ihre Tickets stehen nun zum Download bereit unter:
+https://{$smarty.server.SERVER_NAME}/media/tickets/{$hash}.pdf
+
+Alle Hinweise zur Verwendung finden Sie unterhalb der Tickets.
+
+
+Die Freilichtbühne am schiefen Turm
--- order.php
+++ order.php
@@ -97,11 +97,11 @@ if ($_GET['ajax']) {
 			}
 			
 			if ($ok) {
-				$sId = createId(6, "orders", "sId", true);
+				$bsId = createId(6, "orders", "sId", true);
 				
 				// create order in db
 				$_db->query('INSERT INTO orders VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0, 0, 0, "")',
-						array($sId, $order['address']['firstname'], $order['address']['lastname'], $order['address']['fon'], $order['address']['email'], $order['payment']['method'], $order['payment']['name'], $order['payment']['number'], $order['payment']['blz'], $order['payment']['bank'], ($order['payment']['accepted'] == "true") ? 1 : 0, ($order['accepted'] == "true") ? 1 : 0, $order['total'], time(), $_SERVER['REMOTE_ADDR']));
+						array($bsId, $order['address']['firstname'], $order['address']['lastname'], $order['address']['fon'], $order['address']['email'], $order['payment']['method'], $order['payment']['name'], $order['payment']['number'], $order['payment']['blz'], $order['payment']['bank'], ($order['payment']['accepted'] == "true") ? 1 : 0, ($order['accepted'] == "true") ? 1 : 0, $order['total'], time(), $_SERVER['REMOTE_ADDR']));
 				$orderId = $_db->id();
 				
 				// create ticket in db
@@ -126,6 +126,80 @@ if ($_GET['ajax']) {
 				$header .= "Mime-Version: 1.0 Content-Type: text/plain; charset=utf-8 Content-Transfer-Encoding: quoted-printable";
 				
 				@mail($order['address']['email'], "Ihre Bestellung", $body, $header);
+				
+				if ($order['payment']['method'] == "charge") {
+					$_tpl->assign("hash", md5($orderId));
+					$body = $_tpl->fetch("order_mail_tickets.tpl");
+					
+					@mail($order['address']['email'], "Ihre Karten", $body, $header);
+				}
+				
+				// create pdf document
+				require('/usr/share/php/fpdf/fpdf.php');
+
+				$pdf = new FPDF();
+				$pdf->AddFont("Qlassik", "", "Qlassik_TB.php");
+				$pdf->AddFont("Code39", "", "code39.php");
+				$pdf->AddPage();
+				$pdf->SetAutoPageBreak(false);
+
+				$width = 120;
+				$height = 60;
+				$ticketHeight = $height+1;
+
+				$tickets = 0;
+				$result = $_db->query('SELECT * FROM orders_tickets WHERE `order` = ?', array($orderId));
+				while ($ticket = $result->fetch()) {
+					
+					$orX = 15;
+					$orY = 15 + $tickets * ($ticketHeight + 7);
+
+					if ($orY + $ticketHeight > 280) {
+						$pdf->AddPage();
+						$tickets = 0;
+						$orY = 15;
+					}
+
+					// frame
+					$pdf->SetLineWidth(0.5);
+					$pdf->Rect($orX, $orY, $width, $height, "D");
+					$orX += 0.5 + 2;
+					$orY += 0.5 + 2;
+
+					$pdf->Image("./gfx/logo.png", $orX + 85, $orY, 28, 0, "PNG");
+
+					$pdf->SetXY($orX + 3, $orY + 2);
+					$pdf->SetFont("Qlassik", "", "25");
+					$pdf->Cell(50, 10, "Eintrittskarte", 0, 2);
+					$pdf->SetFont("Qlassik", "", "18");
+					$pdf->Cell(50, 9, ($ticket['type'] == 1) ? "Erwachsener" : "Kind", 0, 2);
+					$pdf->SetFont("Helvetica", "", "12");
+					$pdf->Cell(50, 13, utf8_decode("Aufführung: ".getStringForDate($dates[$ticket['date']])), 0, 2);
+					$pdf->SetFont("Helvetica", "", "18");
+					$pdf->SetX($pdf->GetX() + 4);
+					$pdf->Cell(50, 7, $prices[(($ticket['type'] == 1) ? "adults" : "kids")].",00 ".chr(128), 0, 2);
+
+					$pdf->Line($orX + 2, $orY + 48, $orX + 42, $orY + 48);
+
+					$pdf->setXY($orX + 2, $orY + 44);
+					$pdf->SetFont("Helvetica", "", "9");
+					$pdf->Cell(43, 16, "KN: ".$ticket['sId']." | BN: ".$bsId, 0, 0);
+
+					$pdf->SetFont("Code39", "", "33");
+					$pdf->Cell(50, 10, "*".$ticket['sId']."A".$bsId."*", 0, 2);
+
+					$tickets++;
+					
+				}
+
+				$pdf->SetX(15);
+				$pdf->SetY($pdf->GetY() + 15);
+				$pdf->SetFont("Helvetica", "", "18");
+				$pdf->Cell(50, 10, "Hinweise:", 0, 2);
+				$pdf->SetFont("Helvetica", "", "10");
+				$pdf->MultiCell(0, 6, utf8_decode("Es handelt sich hierbei nur um vorläufige Karten. Sie erhalten gegen Vorlage dieser Ihre endgültigen Karten an der Abendkasse. Drucken Sie sich dazu bitte dieses Dokument aus. Gerne können Sie die Karten auch auseinanderschneiden und einzeln verwenden. Sollten Sie über keine Druckmöglichkeit verfügen, können Sie sich auch alternativ die KN-Nummer (unten links) jeder Karte notieren und an der Abendkasse vorzeigen."), 0, 2);
+
+				$pdf->Output("./media/tickets/".md5($orderId).".pdf");
 			}
 			
 			$response['status'] = ($ok) ? "ok" : "error";
