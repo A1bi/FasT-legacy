0532cfa82171c4db9d6bd7a516540ca2e05a54be
Albrecht Oster <a.oster@online.de>
2012-07-02 09:03:16 +0200

    created general map library which will also work with other projects

--- js/info.js
+++ js/info.js
@@ -1,90 +1,84 @@
-function Map() {
+function Map(id) {
 	
 	var fromProjection = new OpenLayers.Projection("EPSG:4326");
 	var toProjection = new OpenLayers.Projection("EPSG:900913");
 	var icons = [];
-	var mapData = {};
 	var locations = [];
+	var layers = {};
+	var map;
 	
-	var getMapData = function () {
-		$.getJSON("/media/map.json", function (data) {
-			mapData = data;
-			
-			initMap();
-		});
-	}
-	
-	var initIcons = function () {
-		$.each(mapData.icons, function (key, value) {
+	this.registerIcons = function (icns) {
+		$.each(icns, function (key, value) {
 			var size = new OpenLayers.Size(value.size[0], value.size[1]);
 			var offset = new OpenLayers.Pixel(value.offset[0], value.offset[1]);
-			var icon = new OpenLayers.Icon('/gfx/info/'+value.file, size, offset);
+			var icon = new OpenLayers.Icon(value.file, size, offset);
 			
 			icons[value.name] = icon;
 		});
 	}
 	
-	var initLocations = function () {
-		$.each(mapData.locations, function (key, value) {
-			var loc = new OpenLayers.LonLat(value[0], value[1]).transform(fromProjection, toProjection);
-			locations.push(loc);
+	this.registerLocations = function (locs) {
+		$.each(locs, function (key, loc) {
+			var location = new OpenLayers.LonLat(loc[0], loc[1]).transform(fromProjection, toProjection);
+			locations.push(location);
 		});
 	}
 	
-	var addMarkers = function (layer, map) {
-		$.each(mapData.markers, function (key, value) {
+	this.addMarkers = function (markers) {
+		$.each(markers, function (key, value) {
 			var loc = locations[value.loc];
 			
-			var feature = new OpenLayers.Feature(layer, loc); 
-			feature.closeBox = true;
-			feature.popupClass = OpenLayers.Class(OpenLayers.Popup.FramedCloud, {'autoSize': true});
-			feature.data.popupContentHTML = "<b>" + value.title + "</b><br />" + value.desc;
-			feature.data.overflow = "auto";
-		
 			var marker = new OpenLayers.Marker(loc, icons[value.icon].clone());
-			marker.feature = feature;
-			marker.events.register("mousedown", feature, function (event) {
-				if (this.popup == null) {
-					this.popup = this.createPopup(this.closeBox);
-					map.addPopup(this.popup);
-					this.popup.show();
-				} else {
-					this.popup.toggle();
+			layers.markers.addMarker(marker);
+			
+			if (value.bubble) {
+				var feature = new OpenLayers.Feature(layers.markers, loc); 
+				feature.closeBox = true;
+				feature.popupClass = OpenLayers.Class(OpenLayers.Popup.AnchoredBubble, {'autoSize': true});
+				feature.data.popupContentHTML = "<b>" + value.title + "</b><br />" + value.desc;
+				feature.data.overflow = "auto";
+			
+				marker.feature = feature;
+				marker.events.register("mousedown", feature, function (event) {
+					if (this.popup == null) {
+						this.popup = this.createPopup(this.closeBox);
+						map.addPopup(this.popup);
+						this.popup.show();
+					} else {
+						this.popup.toggle();
+					}
+					OpenLayers.Event.stop(event);
+				});
+			}
+			
+		});
+	}
+	
+	this.setCenter = function (loc, zoom) {
+		map.setCenter(locations[loc], zoom);
+	}
+	
+	var addLayer = function (layer, name) {
+		layers[name] = layer;
+		map.addLayer(layer);
+	}
+	
+	
+	map = new OpenLayers.Map(id, {
+		controls: [
+			new OpenLayers.Control.Navigation({
+				mouseWheelOptions: {
+					interval: 50,
+					cumulative: false
 				}
-				OpenLayers.Event.stop(event);
-			});
+			}),
+			new OpenLayers.Control.Attribution()
+		]
+	});
 
-			layer.addMarker(marker);
-		});
-	}
-	
-	var initMap = function () {		
-		initIcons();
-		initLocations();
-		var map = new OpenLayers.Map('map', {
-				controls: [
-					new OpenLayers.Control.Navigation({
-						mouseWheelOptions: {
-							interval: 50,
-							cumulative: false
-						}
-					}),
-					new OpenLayers.Control.Attribution()
-				]
-		});
+	addLayer(new OpenLayers.Layer.OSM(), "osm");
+	addLayer(new OpenLayers.Layer.Markers("markers"), "markers");
 
-		var osm = new OpenLayers.Layer.OSM();
-		
-		var markers = new OpenLayers.Layer.Markers("markers");
-		addMarkers(markers, map);
-
-		map.addLayers([osm, markers]);
-		map.setCenter(locations[mapData.centerLoc], mapData.zoom);	
-	}
-	
-	this.init = function () {
-		getMapData();
-	}
 }
 
 function Weather() {
@@ -124,8 +118,23 @@ function Weather() {
 }
 
 $(function () {
-	var map = new Map();
-	map.init();
+	$.getJSON("/media/map.json", function (data) {
+		var map = new Map("map");
+		
+		$.each(data.icons, function (key, value) {
+			data.icons[key].file = '/gfx/info/' + value.file;
+		});
+		map.registerIcons(data.icons);
+		
+		map.registerLocations(data.locations);
+		
+		$.each(data.markers, function (key, value) {
+			data.markers[key].bubble = true;
+		});
+		map.addMarkers(data.markers);
+		
+		map.setCenter(data.centerLoc, data.zoom);
+	});
 	
 	var weather = new Weather();
 	weather.init();
