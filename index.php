1895676dc63394c9970d2479c0b7d2df20c16fee
Albrecht Oster <a.oster@online.de>
2012-07-31 00:35:35 +0200

    bugfix

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 include('include/main.php');
 
 if (empty($_GET['page'])) $_GET['page'] = "index";
-if (!file_exists("./include/templates/".$_GET['page'].".tpl")) {
+
+if (!$_tpl->templateExists($_GET['page'] . ".tpl")) {
 	echo "Seite nicht gefunden!";
+
 } else {
 	$_tpl->display($_GET['page'].".tpl");
 }
+
 ?>
