c73fcc907a6955bcf993fd51d149183faf09f01a
Albrecht Oster <a.oster@online.de>
2012-09-01 17:48:48 +0200

    new API

--- api.php
+++ api.php
@@ -4,62 +4,61 @@ include('./include/main.php');
 $_db = new database;
 loadComponent("orderManager");
 
+$request = json_decode(file_get_contents("php://input"));
 $response = array();
 
-if ($_GET['order']) {
-	
-} else {
-	switch ($_GET['action']) {
-		case "getOrders":
-			if (!$_GET['date']) $_GET['date'] = 1;
+switch ($request->action) {
+	case "getOrders":
+		if (!$request->date) $request->date = 1;
+		
+		$result = $_db->query('SELECT o.id FROM orders AS o, orders_tickets AS t WHERE t.date = ? AND o.id = t.order GROUP BY o.id', array($request->date));
+		$orders = array();
+		while ($row = $result->fetch()) {
+			$order = OrderManager::getOrderById($row['id']);
 			
-			$result = $_db->query('SELECT o.id FROM orders AS o, orders_tickets AS t WHERE t.date = ? AND o.id = t.order GROUP BY o.id', array($_GET['date']));
-			$orders = array();
-			while ($row = $result->fetch()) {
-				$order = OrderManager::getOrderById($row['id']);
-				
-				$tickets = array();
-				foreach ($order->getTickets() as $ticket) {
-					$tickets[] = array(
-						"id" => $ticket->getId(),
-						"sId" => $ticket->getSId(),
-						"type" => $ticket->getType(),
-						"voided" => $ticket->getVoided()
-					);
-				}
-				
-				$address = $order->getAddress();
-				$payment = $order->getPayment();
-				
-				$orders[] = array(
-					"id" => $order->getId(),
-					"sId" => $order->getSId(),
-					"type" => $order->getType(),
-					"total" => $order->getTotal(),
-					"address" => array(
-						"firstname" => $address['firstname'],
-						"lastname" => $address['lastname'],
-						"affiliation" => $address['affiliation']
-					),
-					"payMethod" => $payment['method'],
-					"paid" => (bool)$order->isPaid(),
-					"cancelled" => array(
-						"cancelled" => (bool)$order->isCancelled(),
-						"cancelReason" => $order->getCancelReason()
-					),
-					"notes" => $order->getNotes(),
-					"tickets" => $tickets
+			$tickets = array();
+			foreach ($order->getTickets() as $ticket) {
+				$tickets[] = array(
+					"id" => $ticket->getId(),
+					"sId" => $ticket->getSId(),
+					"type" => $ticket->getType(),
+					"voided" => $ticket->getVoided()
 				);
 			}
 			
-			$response['status'] = "ok";
-			$response['orders'] = $orders;
-			break;
+			$address = $order->getAddress();
+			$payment = $order->getPayment();
 			
-		case "void":
-			$_db->query('UPDATE orders_tickets SET voided = ? WHERE id = ?', array(time(), $_GET['ticket']));
-			break;
-	}
+			$orders[] = array(
+				"id" => $order->getId(),
+				"sId" => $order->getSId(),
+				"type" => $order->getType(),
+				"total" => $order->getTotal(),
+				"address" => array(
+					"firstname" => $address['firstname'],
+					"lastname" => $address['lastname'],
+					"affiliation" => $address['affiliation']
+				),
+				"payMethod" => $payment['method'],
+				"paid" => (bool)$order->isPaid(),
+				"cancelled" => array(
+					"cancelled" => (bool)$order->isCancelled(),
+					"cancelReason" => $order->getCancelReason()
+				),
+				"notes" => $order->getNotes(),
+				"tickets" => $tickets
+			);
+		}
+		
+		$response['status'] = "ok";
+		$response['orders'] = $orders;
+		break;
+		
+	case "voidTickets":
+		foreach ($request->tickets as $ticket) {
+			$_db->query('UPDATE orders_tickets SET voided = ? WHERE id = ?', array($ticket->voided, $ticket->id));
+		}
+		break;
 }
 
 echo json_encode($response);
